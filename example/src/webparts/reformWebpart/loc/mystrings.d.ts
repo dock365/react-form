92660b65bbc13dcc7c63bd1428f1495238f25fb1
Hafeez Hamza <hafeez.hamza@global-infonet.com>
2019-06-30 17:10:45 +0530

    Example

---
+++ example/src/webparts/reformWebpart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IReformWebpartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ReformWebpartWebPartStrings' {
+  const strings: IReformWebpartWebPartStrings;
+  export = strings;
+}
